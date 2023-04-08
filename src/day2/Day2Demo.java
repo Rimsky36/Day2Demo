add7b16fcf7478b244b512b7ae1de5269ebf4d30
Rimsky <Rimsky@192.168.1.145>
2023-04-07 17:30:06 -0700

    adding new getInfo()method

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -1,10 +1,16 @@
 package day2;
 
 public class Day2Demo {
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Day 2 Git");
-	}
+	
+	
+	
+	public void getInfo() {
+		System.out.println("Pushing code...");
+		System.out.println("Pushing code 2nd time...");
 
+		
+		
+	}
+	
+	
 }
