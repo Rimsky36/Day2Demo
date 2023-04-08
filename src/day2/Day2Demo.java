d72f43410c89f19481c6ec686f06dd50c10d4721
Rimsky <Rimsky@192.168.1.145>
2023-04-07 17:56:40 -0700

    commit implement

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -1,12 +1,15 @@
 package day2;
 
 public class Day2Demo {
+	public static void main(String[] args) {
+		
+	}
 	
 	
 	
 	public void getInfo() {
 		System.out.println("Pushing code...");
-		System.out.println("Pushing code 2nd time...");
+	
 
 		
 		
